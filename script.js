4e3b7f6109471a815e84ce137fc4311007835f69
Mishaproh96 <mishaprohorenko96@gmail.com>
2021-04-11 18:19:00 +0300

    Object.entries

--- script.js
+++ script.js
@@ -1,2 +1,42 @@
 'use strict'
 
+let persons = [
+    {
+        name: 'John',
+        age: 42,
+        isDev: false,
+    },
+    {
+        name: 'Rick',
+        age: 22,
+        isDev: false,
+        skills: [
+            'React'
+        ]
+    },
+    {
+        name: 'Kolya',
+        age: 15,
+        isDev: true,
+    },
+    {
+        name: 'Gerg',
+        age: 12,
+        isDev: false,
+    },
+    {
+        name: 'David',
+        age: 24,
+        isDev: true,
+    },
+];
+
+const obj = persons[1]
+
+let resultOfObjectEntries = Object.entries(obj)
+
+console.log(resultOfObjectEntries)
+
+let resultFromObjectEntries = Object.fromEntries(resultOfObjectEntries)
+
+console.log(resultFromObjectEntries)
